cec4bb6b69add177829b908c86f1fd2fd26362d5
JeppeOEM <jappa669@gmail.com>
2026-01-05 15:38:54 +0100

    resolving result promise in controller

--- src/controllers/analyze.controller.ts
+++ src/controllers/analyze.controller.ts
@@ -2,12 +2,22 @@ import { GitRepositoryService } from "../services/analyze.service.js";
 import { Request, Response } from "express";
 
 export class GitRepositoryController {
-  constructor(private gitRepositoryService: GitRepositoryService) {}
+  constructor(private gitRepositoryService: GitRepositoryService) { }
 
-  getGitRepositories(req: Request, res: Response): void {
-    const username = req.params.username;
-    console.log("Username:", username);
-    const result = this.gitRepositoryService.getGitRepositories(username);
-    res.json(result);
+  async getGitRepositories(req: Request, res: Response): Promise<void> {
+    try {
+      const username = req.params.username;
+      console.log("Username:", username);
+
+      const result = await this.gitRepositoryService.getGitRepositories(username);
+
+      console.log("Result:", result);
+      res.status(200).json(result);
+    } catch (err) {
+      console.error(err);
+      res.status(500).json({
+        message: "Failed to analyze GitHub repositories",
+      });
+    }
   }
 }
